ddba43f176ab262742bc5bedaea4a68ffec2c205
Hectoresen <inforeshector@gmail.com>
2022-09-13 09:45:56 +0200

    FEAT: Preparing components and dependencies for authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User } from 'src/users/entities/user.entity';
+import { UserSchema } from 'src/users/schema/users.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      }
+    ]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
